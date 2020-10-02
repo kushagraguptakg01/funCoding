3f95de8ce949df3ab3d21b0f7a13095bc593c60c
Mohammed Mohsin Hussain <md.mohsin.hussain786@gmail.com>
2020-10-02 23:56:44 +0530

    Added Java Code

---
+++ fun.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Fun {
+	public static void main (String[] args) {
+		int a = Integer.parseInt(args[2]);
+		ArrayList<Integer> arr = new ArrayList();
+		for (int i=a; i!=0; i/=10) {
+		    int x = a%10;
+		    v.add(x);
+		    cout<<x<<" \n";
+		}
+		
+	}
+}
